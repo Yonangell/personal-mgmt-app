d25e8618592f14ff8261d262dcc40c8499a08fc8
Yonangell Guillen <121691054+Yonangell@users.noreply.github.com>
2026-03-07 21:17:34 +0100

    Agregando informacion

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <title>Plataforma de Gestión Personal</title>
+        <link rel="icon" type="image/x-icon" href="/assets/image/computacion-en-la-nube.png"/>
+      </head>
+      <head>
       </head>
       <body>
         {children}
